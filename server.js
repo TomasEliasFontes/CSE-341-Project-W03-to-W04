aed29f8f827f2f1e24c2f5c3041e57ca8d43539a
tomaseliasfontes <tomaseliasfontes@gmail.com>
2025-07-22 09:27:07 -0300

    add comment in page

--- server.js
+++ server.js
@@ -18,5 +18,9 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.get('/', (req, res) => {
+  res.send('🎬 Movie & Review API is running');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`API running on port ${port}`));
