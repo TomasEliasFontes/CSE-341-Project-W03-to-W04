16e95df0c9efa88816ce728ed182a493e2eaa601
tomaseliasfontes <tomaseliasfontes@gmail.com>
2025-07-28 20:04:06 -0300

    Const to Auth, Pass and Google OAuth

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const express       = require('express');
 const swaggerUi     = require('swagger-ui-express');
 const swaggerDoc    = require('./swagger.json');
 const connectDB     = require('./data/database');
+const requireAuth   = require('./middleware/auth');
+const User = require('./models/user');
+
+const session  = require('express-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+// Conect to MongoDB 
+require('dotenv').config();
 
 const app           = express();
 const port          = process.env.PORT || 3000;
 
-// Conect to MongoDB 
-require('dotenv').config();
 connectDB()
   .then(() => console.log('✅ Connected to MongoDB'))
   .catch(err => {
@@ -16,12 +23,38 @@ connectDB()
     process.exit(1);
   });
 
+// Middleware for password
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Strategie for google 
+passport.use(new GoogleStrategy({
+  clientID:     process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL:  '/auth/google/callback'
+}, async (accessToken, refreshToken, profile, done) => {
+  // Aquí buscas o creas usuario en DB usando profile.id
+  let user = await User.findOne({ googleId: profile.id });
+  if (!user) {
+    user = await User.create({ googleId: profile.id, email: profile.emails[0].value });
+  }
+  done(null, user);
+}));
+
+passport.serializeUser((user, done) => done(null, user._id));
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
+
 //Middleware for JSON
 app.use(express.json());
+app.use('/auth', require('./routes/auth'));
 
 //Mount the routers
-app.use('/movies',  require('./routes/movies'));
-app.use('/reviews', require('./routes/reviews'));
+app.use('/movies',  requireAuth, require('./routes/movies'));
+app.use('/reviews', requireAuth, require('./routes/reviews'));
 
 //Use of Swagger UI routes in the /api-docs
 app.use(
